ee36494002b48f7e2f68f726f1d651fd80678566
unknown <seyedolmohadesin.s@northeastern.edu>
2023-06-08 08:34:12 -0400

    Remove main() and docopt

--- cfm/zmq/client_with_gui.py
+++ cfm/zmq/client_with_gui.py
@@ -1,27 +1,9 @@
 #! python
 #
 # Copyright 2021
-# Author: Mahdi Torkashvand, Vivek Venkatachalam
-
-"""
-ZMQ Client.
-
-Usage:
-    client.py             [options]
-
-Options:
-    -h --help             Show this help.
-    --port=PORT           [default: 5002]
-    --port_forwarder_in=PORT           [default: L5000]
-"""
-
-DEBUG = True
-
-import time
-import signal
+# Author: Mahdi Torkashvand, Sina Rasouli
 
 import zmq
-from docopt import docopt
 
 from cfm.zmq.publisher import Publisher
 from cfm.zmq.utils import parse_host_and_port
@@ -53,7 +35,7 @@ class GUIClient():
         self.control_socket = self.context.socket(zmq.PUB)
         self.control_socket.bind("tcp://*:4862")  # DEBUG TODO change this to an argument for the class
 
-        self.forwarder_publisher = Publisher(
+        self.publisher = Publisher(
             host=self.bounds_forwarder_in[0],
             port=self.bounds_forwarder_in[1],
             bound=self.bounds_forwarder_in[2]
@@ -70,43 +52,12 @@ class GUIClient():
         self.socket.send(req)
     
     def log(self, msg):
-        self.forwarder_publisher.send("logger "+ str(msg))
+        self.publisher.send("logger "+ str(msg))
 
     def process(self, req_str):
-        """Take a single request from stdin, send
-        it to a server, and return the reply."""
-        if not self.running:
-            return
         self.send(coerce_bytes(req_str))
         self.log(f"<CLIENT WITH GUI> command sent: {req_str}")
-        if not DEBUG:
-            resp_str = coerce_string(self.recv())
-            print(resp_str)
-            self.log(f"<CLIENT WITH GUI> response received: {resp_str}")
+        rep_str = coerce_string(self.recv())
+        self.log(f"<CLIENT WITH GUI> response received: {rep_str}")
         if req_str == "DO shutdown":
-            time.sleep(1)
             self.control_socket.send_string("TERMINATE")
-
-
-    def run(self):
-        """Start looping."""
-
-        self.running = True
-        def _finish(*_):
-            raise SystemExit
-
-        signal.signal(signal.SIGINT, _finish)
-
-def main():
-    """CLI entry point."""
-
-    args = docopt(__doc__)
-    port = int(args["--port"])
-    port_forwarder_in = args["--port_forwarder_in"]
-
-    client = GUIClient(port=port, port_forwarder_in=port_forwarder_in)
-    client.run()
-
-
-if __name__ == "__main__":
-    main()
