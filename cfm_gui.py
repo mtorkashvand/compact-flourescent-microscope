57c096e1ccfa070f7c20233d6b16887fe78eaeec
unknown <seyedolmohadesin.s@northeastern.edu>
2023-06-08 10:22:29 -0400

    Update cfm_gui.py

--- cfm_gui.py
+++ cfm_gui.py
@@ -17,26 +17,43 @@
 
 # Modules
 import time
-
 from collections import defaultdict
 
-import numpy as np
 import cv2 as cv
-
+import numpy as np
 import PySimpleGUI as sg
 
 from cfm.zmq.client_with_gui import GUIClient
 from cfm.system.cfm_with_gui import CFMwithGUI
+from cfm.devices.utils import array_props_from_string
 from cfm.devices.dual_displayer import DualDisplayer
 from cfm.ui.elements import (
-    InputSlider, CombosJoined, InputWithIncrements, ReturnHandler,
+    InputWithIncrements, ReturnHandler,
     LEDCompound, ExposureCompound, FramerateCompound, LEDIR,
-    ZInterpolationTracking
+    ZInterpolationTracking, ToggleRecording, ToggleTracking
+)
+
+from cfm.icons.icons import (
+    ICON_LED_GFP_OFF, ICON_LED_GFP_ON, ICON_LED_OPT_OFF, ICON_LED_OPT_ON,
+    ICON_LED_IR_ON, ICON_LED_IR_OFF, ICON_EXPOSURE, ICON_FPS,
+    ICON_RECORDING_OFF, ICON_RECORDING_ON,
+    ICON_TRACKING_OFF, ICON_TRACKING_ON
 )
 
 # Parameters
 DEBUG = True
-
+ICON_SIZE = (64, 64)
+# These numbers are subject to change based on the camera model and its orientation when mounted.
+CAMERA_X_MAX = 1920
+CAMERA_Y_MAX = 1200
+default_b_x_offset = -10
+default_b_y_offset = -44
+default_g_x_offset = -36
+default_g_y_offset = 44
+offset_step_small = 2
+offset_step_large = 10
+fmt = "UINT8_YX_512_512"
+(_, _, shape) = array_props_from_string(fmt)
 forwarder_in = str(5000)
 forwarder_out = str(5001)
 server_client = str(5002)
@@ -58,6 +75,9 @@ exposure_behavior = 1
 exposure_gcamp = 1
 interpolation_tracking = False
 
+y_bound = int((CAMERA_Y_MAX - binsize * shape[0]) / (2 * binsize))
+x_bound = int((CAMERA_X_MAX - binsize * shape[1]) / (2 * binsize))
+
 # Methods
 ## Run CFM with GUI
 def run_cfm_with_gui(**kwargs):
@@ -66,13 +86,7 @@ def run_cfm_with_gui(**kwargs):
         **kwargs
     )
     return cfm_with_gui
-## 
-def sg_input_port(key, port):
-    return sg.Input(
-        key=key,
-        size=5,
-        default_text=str(port)
-    )
+##
 
 # Classes
 
@@ -87,108 +101,142 @@ def sg_input_port(key, port):
 # TODO: check if using `Column Element` will help organizing
 # TODO: use `Image Element` with `update` to show frames?
 # TODO: use `Menu Element` to add menu bars on top of the window
-# TODO: 
+# TODO:
 
+elements = []
 
-ui_binsize_format = CombosJoined(
-    text1="Binsize: ", text2="Format: ",
-    v1_to_v2s={
-        "1": [
-            "UINT8_YX_1024_1024",
-            "UINT8_YX_512_512",
-            "UINT8_YX_256_256",
-            "UINT8_YX_128_128",
-        ],
-        "2": [
-            "UINT8_YX_512_512",
-            "UINT8_YX_256_256",
-            "UINT8_YX_128_128",
-        ],
-        "4": [
-            "UINT8_YX_256_256",
-            "UINT8_YX_128_128",
-        ],
-    },
-    default_v1="2", default_v2="UINT8_YX_512_512",
-    key1="binsize", key2="format"
+ui_return_handler = ReturnHandler()
+elements.append(ui_return_handler)
+
+ui_recording_toggle = ToggleRecording(
+    key="recording",
+    icon_off=ICON_RECORDING_OFF,
+    icon_on=ICON_RECORDING_ON,
+    icon_size=ICON_SIZE
 )
+elements.append(ui_recording_toggle)
+
+ui_tracking_toggle = ToggleTracking(
+    key="tracking",
+    icon_off=ICON_TRACKING_OFF,
+    icon_on=ICON_TRACKING_ON,
+    icon_size=ICON_SIZE
+)
+elements.append(ui_tracking_toggle)
+
 ui_offset_behavior_x = InputWithIncrements(
     text = "Offset Behavior X: ",
     key="offset_behavior_x",
-    default_value=-10,
-    bounds=[-224, 224],
-    increments=[-10,-2,2,10],
+    default_value=default_b_x_offset,
+    bounds=[-x_bound, x_bound],
+    increments=[-offset_step_large,
+                -offset_step_small,
+                offset_step_small,
+                offset_step_large],
     type_caster=int
 )
+elements.append(ui_offset_behavior_x)
+
 ui_offset_behavior_y = InputWithIncrements(
     text = "Offset Behavior Y: ",
     key="offset_behavior_y",
-    default_value=-44,
-    bounds=[-44, 44],
-    increments=[-10,-2,2,10],
+    default_value=default_b_y_offset,
+    bounds=[-y_bound, y_bound],
+    increments=[-offset_step_large,
+                -offset_step_small,
+                offset_step_small,
+                offset_step_large],
     type_caster=int
 )
+elements.append(ui_offset_behavior_y)
+
 ui_offset_gcamp_x = InputWithIncrements(
     text = "Offset GCaMP X: ",
     key="offset_gcamp_x",
-    default_value=-36,
-    bounds=[-224, 224],
-    increments=[-10,-2,2,10],
+    default_value=default_g_x_offset,
+    bounds=[-x_bound, x_bound],
+    increments=[-offset_step_large,
+                -offset_step_small,
+                offset_step_small,
+                offset_step_large],
     type_caster=int
 )
+elements.append(ui_offset_gcamp_x)
+
 ui_offset_gcamp_y = InputWithIncrements(
     text = "Offset GCaMP Y: ",
     key="offset_gcamp_y",
-    default_value=44,
-    bounds=[-44, 44],
-    increments=[-10,-2,2,10],
+    default_value=default_g_y_offset,
+    bounds=[-y_bound, y_bound],
+    increments=[-offset_step_large,
+                -offset_step_small,
+                offset_step_small,
+                offset_step_large],
     type_caster=int
 )
+elements.append(ui_offset_behavior_y)
 
-ui_return_handler = ReturnHandler()
 ui_led_gfp = LEDCompound(
-    button_text='led-G', text='LED GFP', key='led_gfp', led_name='g',
-    bounds=(0,255)
+    text="470nm LED power (%)",
+    key='led_gfp',
+    led_name='g',
+    icon_off=ICON_LED_GFP_OFF,
+    icon_on=ICON_LED_GFP_ON,
+    icon_size=ICON_SIZE,
+    bounds=(0, 100)
 )
+elements.append(ui_led_gfp)
+
 ui_led_opt = LEDCompound(
-    button_text='led-O', text='LED Optogenetics', key='led_opt', led_name='o',
-    bounds=(0,255)
+    text="595nm LED power (%)",
+    key='led_opt',
+    led_name='o',
+    icon_off=ICON_LED_OPT_OFF,
+    icon_on=ICON_LED_OPT_ON,
+    icon_size=ICON_SIZE,
+    bounds=(0, 100)
 )
-ui_led_ir = LEDIR()
+elements.append(ui_led_opt)
+
+ui_led_ir = LEDIR(
+    text="760nm LED power (%)",
+    key='led_ir',
+    icon_off=ICON_LED_IR_OFF,
+    icon_on=ICON_LED_IR_ON,
+    icon_size=ICON_SIZE,
+)
+elements.append(ui_led_ir)
+
 ui_exposure_gfp = ExposureCompound(
-    button_text='exposure-gcamp', text='Exposure GFP',
+    text='Exposure Time',
     key='exposure_gcamp',
+    icon=ICON_EXPOSURE,
+    icon_size=ICON_SIZE,
     camera_name='gcamp',
     bounds=(250, 478000)
 )
+elements.append(ui_exposure_gfp)
+
 ui_exposure_behavior = ExposureCompound(
-    button_text='exposure-behavior', text='Exposure Behavior',
+    text='Exposure Time',
     key='exposure_behavior',
+    icon=ICON_EXPOSURE,
+    icon_size=ICON_SIZE,
     camera_name='behavior',
     bounds=(250, 478000)
 )
+elements.append(ui_exposure_behavior)
+
 ui_framerate = FramerateCompound(
-    ui_exposure_behavior, ui_exposure_gfp,
+    ui_exposure_behavior,
+    ui_exposure_gfp,
+    icon=ICON_FPS,
+    icon_size=ICON_SIZE
 )
+elements.append(ui_framerate)
+
 ui_interpolation_tracking = ZInterpolationTracking()
-
-
-# Add Elements
-elements = [
-    ui_return_handler,
-    ui_led_gfp, ui_led_opt, ui_led_ir,
-    ui_framerate, ui_binsize_format,
-    ui_offset_behavior_x, ui_offset_behavior_y,
-    ui_offset_gcamp_x, ui_offset_gcamp_y,
-    ui_exposure_gfp, ui_exposure_behavior,
-    ui_interpolation_tracking,
-]
-
-
-
-
-
-
+elements.append(ui_interpolation_tracking)
 
 folder_browser_data = sg.FolderBrowse(
     button_text = "Browse",
@@ -200,80 +248,49 @@ folder_browser_logger = sg.FolderBrowse(
     target = "logger_directory",
     initial_folder = "."
 )
-progress_bar = sg.ProgressBar(
-    max_value=100,
-    size=(100,5),
-    orientation='horizontal',
-    visible=True,
-    key='progressbar'
+
+led_column_layout = sg.Column(
+    [[*ui_led_ir.elements], 
+     [*ui_led_gfp.elements], 
+     [*ui_led_opt.elements]]
 )
+exp_fps_column_layout = sg.Column(
+    [[*ui_framerate.elements],
+     [*ui_exposure_behavior.elements],
+     [*ui_exposure_gfp.elements]]
+)
+
 layout = [
     [
         *ui_return_handler.elements,
-    ],
-    # Start & Stop
-    [
-        [
-            sg.Button('Start'),
-            sg.Button('Stop'),
-            sg.Button('Start Recording', disabled=False),
-            sg.Button('Stop Recording', disabled=True),
-        ],
-    ],
-    [
+    ],[
+        *ui_recording_toggle.elements, *ui_tracking_toggle.elements
+    ],[
         sg.HorizontalSeparator(),
-    ],
-    # Camera Lighting
-    [
-        *ui_framerate.elements, *ui_exposure_gfp.elements, *ui_exposure_behavior.elements,
-    ],
-    # Camera Binning and Size
-    [
-        *ui_binsize_format.elements
-    ],
-    [
-        sg.Text("teensy_usb_port "), sg_input_port("teensy_usb_port", "COM4"),
+    ],[
+        [exp_fps_column_layout, sg.VSeparator(), led_column_layout, sg.VSeparator()]
+    ],[
+        sg.HorizontalSeparator(),
+    ],[
         *ui_interpolation_tracking.elements,
     ],[
         sg.Text("data_directory: "),
         folder_browser_data, sg.Input(key="data_directory", default_text=r"./", size=30),
         sg.Text("logger_directory: "),
         folder_browser_logger, sg.Input(key="logger_directory", default_text=r"./", size=30),
-    ],
-    [
+    ],[
         sg.HorizontalSeparator(),
-    ], [  # LED Controls
-        *ui_led_ir.elements,
-        *ui_led_gfp.elements,
-        *ui_led_opt.elements,
-    ],
-    [
-        sg.Image(key="img_frame_r", size=(512, 512)),
-        sg.Image(key="img_frame_g", size=(512, 512)),
+    ],[
+        sg.Image(key="img_frame_r", size=shape),
+        sg.Image(key="img_frame_g", size=shape),
     ],[
         *ui_offset_behavior_x.elements, *ui_offset_behavior_y.elements,
     ],[
         *ui_offset_gcamp_x.elements, *ui_offset_gcamp_y.elements,
     ],[
         sg.HorizontalSeparator(),
-    ],
-    [
-        sg.Button('Ok'),
-        sg.Button('Quit'),
-        sg.Button('Execute'),
-        sg.Input(default_text="DO shutdown", size=50, key="client_cli"),
-    ],
-    [
-        progress_bar
-    ],
-    
-    [
-        sg.StatusBar(
-            text="Status! :wink:",
-            size=(20,1),
-            key="statusbar"
-        ),
-        sg.Sizegrip()
+    ],[
+        sg.Button('Quit')
     ]
 ]
 
@@ -300,12 +317,12 @@ dual_displayer = DualDisplayer(
     window=window,
     data_r=f"L{tracker_out_behavior}",  # displayer port for the behavior data
     data_g=f"L{tracker_out_gcamp}",  # displayer port for the gcamp data
-    fmt="UINT8_YX_512_512",  # image format accroding to 'array_props_from_string'
+    fmt=fmt,  # image format accroding to 'array_props_from_string'
     name="displayer"  # image displayers name start with 'displayer' 
     )
 
 def zero_displayers():
-    _tmp = np.zeros((512,512),dtype=np.uint8)
+    _tmp = np.zeros(shape, dtype=np.uint8)
     _tmp = cv.imencode('.png', _tmp)[1].tobytes()
     window['img_frame_r'].update(data=_tmp)
     window['img_frame_g'].update(data=_tmp)
@@ -313,19 +330,34 @@ def zero_displayers():
 zero_displayers()
 # Display and interact with the Window using an Event Loop
 # DEBUG TODO:  change code to have different `elements`` for each functionality, e.g. like CombosJoined
-N = 0
-_n, _duration = 0, 0.0
-CAMERA_RUNNING = False
+
+event, values = window.read(timeout=0)
+
+offset_bx = x_bound + int(values['offset_behavior_x'])
+offset_by = y_bound + int(values['offset_behavior_y'])
+offset_gx = x_bound + int(values['offset_gcamp_x'])
+offset_gy = y_bound + int(values['offset_gcamp_y'])
+
+
+cfm_with_gui = run_cfm_with_gui(**values)
+cfm_with_gui.run()
+
+client_cli_cmd = "DO _flir_camera_set_region_behavior 1 {} {} {} {} {}".format(
+    shape[0], shape[1], binsize, offset_by, offset_bx
+)
+gui_client.process(client_cli_cmd)
+
+client_cli_cmd = "DO _flir_camera_set_region_gcamp 1 {} {} {} {} {}".format(
+    shape[0], shape[1], binsize, offset_gy, offset_gx
+)
+gui_client.process(client_cli_cmd)
+
 while True:
-    if CAMERA_RUNNING:
-        event, values = window.read(timeout=10)
-        img_r, img_g, img_combined = dual_displayer.get_frame(combine=True)
-        frame_r = cv.imencode('.png', img_combined)[1].tobytes()
-        frame_g = cv.imencode('.png', img_g)[1].tobytes()
-        window['img_frame_r'].update(data=frame_r)
-        window['img_frame_g'].update(data=frame_g)
-    else:
-        event, values = window.read()
+    event, values = window.read(timeout=10)
+    if event == sg.WIN_CLOSED or event == 'Quit':
+        gui_client.process("DO shutdown")
+        break
+        
     # Add Values
     # To be used by handlers.
     for element in elements:
@@ -337,85 +369,12 @@ while True:
     # Add values from UI element with expected keys in CFMwithGUI
     for element in elements:
         element.add_values(values)
-    if DEBUG:
-        print(values)
-        print(event)
-    # See if user wants to quit or window was closed
-    if event == sg.WINDOW_CLOSED or event == 'Quit':
-        zero_displayers()
-        CAMERA_RUNNING = False
-        gui_client.running = False
-        break
-    elif event == 'Start':
-        # Run CLI Client
-        CAMERA_RUNNING = True
-        gui_client.running = True
-        # Run CFM
-        if 'cfm_with_gui' not in locals():
-            cfm_with_gui = run_cfm_with_gui(**values)
-            cfm_with_gui.run()
-        else:
-            print('Already running!')
-    elif event == 'Stop':
-        # Stop CLI Client
-        zero_displayers()
-        CAMERA_RUNNING = False
-        gui_client.process("DO shutdown")
-        dual_displayer.reset_buffers()
-        time.sleep(0.5)
-        gui_client.running = False
-        # Stop CFM
-        if 'cfm_with_gui' in locals():
-            cfm_with_gui.kill()
-            if DEBUG:
-                print("DEBUG cfm killed")
-            del cfm_with_gui
-            print("Process should be killed.")
-        else:
-            print('Not running!')
-    elif event == 'Execute':
-        client_cli_cmd = values['client_cli']
-        print(f"Executing: '{client_cli_cmd}'")
-        gui_client.process(client_cli_cmd)
-    elif event.startswith("offset_behavior"):
-        offset_bx = 224 + values['offset_behavior_x']
-        offset_by = 44 + values['offset_behavior_y']
-        client_cli_cmd = "DO _flir_camera_set_region_behavior 1 512 512 2 {} {}".format(
-            offset_by, offset_bx
-        )
-        print(f"Executing: '{client_cli_cmd}'")
-        gui_client.process(client_cli_cmd)
-    elif event.startswith("offset_gcamp"):
-        offset_gx = 224 + values['offset_gcamp_x']
-        offset_gy = 44 + values['offset_gcamp_y']
-        client_cli_cmd = "DO _flir_camera_set_region_gcamp 1 512 512 2 {} {}".format(
-            offset_gy, offset_gx
-        )
-        print(f"Executing: '{client_cli_cmd}'")
-        gui_client.process(client_cli_cmd)
-    elif event == 'Start Recording':
-        for element in elements:
-            element.disable()
-        window['Start Recording'].update(disabled=True)
-        window['Stop Recording'].update(disabled=False)
-        client_cli_cmd = f"DO _writer_start"
-        print(f"Executing: '{client_cli_cmd}'")
-        gui_client.process(client_cli_cmd)
-    elif event == 'Stop Recording':
-        for element in elements:
-            element.enable()
-        window['Start Recording'].update(disabled=False)
-        window['Stop Recording'].update(disabled=True)
-        client_cli_cmd = f"DO _writer_stop"
-        print(f"Executing: '{client_cli_cmd}'")
-        gui_client.process(client_cli_cmd)
-    
-    # Output a message to the window
-    N = min(N+1, 100)
-    window.find_element(key="progressbar").update(
-        current_count = N
-    )
 
+    img_r, img_g, img_combined = dual_displayer.get_frame(combine=True)
+    frame_r = cv.imencode('.png', img_combined)[1].tobytes()
+    frame_g = cv.imencode('.png', img_g)[1].tobytes()
+    window['img_frame_r'].update(data=frame_r)
+    window['img_frame_g'].update(data=frame_g)
 # Finish up by removing from the screen
 window.close()
 
@@ -435,7 +394,12 @@ window.close()
 # - [ ] start/stop tracking same button -> change icon
 # - [ ] display radio buttons: overlay or single
 # - [ ] vertical offset controllers
+# - [ ] crashes after the second stop/start
+# - [X] detect the teensy port automatically, and get rid of its element
+# - [ ] changing format brings the binsize back to 1 (can't set it to anything other than 1)
+# - [ ] bounds for the 'inputwithincrement' should depend on the binsize and the format
+
 
 
 if __name__ == '__main__':
-    print('It Worked!')
+    pass
